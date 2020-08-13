475db099d463f5e838ec40fdb9f2ba07f7dcf201
Roi Kingon <rkingon@MacBook-Pro.local>
2020-08-13 09:00:00 -0700

    Mq component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { React, PropTypes, Helmet } from '@/vendor'
-import { Grid } from '@/components'
+import { Grid, Mq } from '@/components'
 import { Header, Subscribe, Footer } from '@/components/AppLayout'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -15,47 +15,57 @@ import LegalRoute from '@/routes/legal'
 
 class App extends React.PureComponent {
 
+	get breakpoints () {
+		return {
+			small: 'screen and (max-width: 767px)',
+			medium: 'screen and (min-width: 768px) and (max-width: 999px)',
+			large: 'screen and (min-width: 1000px)'
+		}
+	}
+
 	render () {
 		return (
 			<BrowserRouter>
-				<Helmet titleTemplate="%s :: Lightwell Survey Wines" />
-				<Grid.Container>
-					<Grid flex="0">
-						<Grid.Cell>
-							<Grid.Content>
-								<Header />
-							</Grid.Content>
-						</Grid.Cell>
-					</Grid>
-					<Grid>
-						<Grid.Cell>
-							<Routes>
-								<Route path="/" element={<IndexRoute />} />
-								<Route path="/shop" element={<ShopRoute />} />
-								<Route path="/about" element={<AboutRoute />} />
-								<Route path="/visit" element={<VisitRoute />} />
-								<Route path="/notes" element={<NotesRoute />} />
-								<Route path="/notes/:slug" element={<NotesSlugRoute />} />
-								<Route path="/contact" element={<ContactRoute />} />
-								<Route path="/legal" element={<LegalRoute />} />
-							</Routes>
-						</Grid.Cell>
-					</Grid>
-					<Grid flex="0">
-						<Grid.Cell>
-							<Grid.Content>
-								<Subscribe />
-							</Grid.Content>
-						</Grid.Cell>
-					</Grid>
-					<Grid flex="0">
-						<Grid.Cell>
-							<Grid.Content>
-								<Footer />
-							</Grid.Content>
-						</Grid.Cell>
-					</Grid>
-				</Grid.Container>
+				<Mq.Provider config={this.breakpoints}>
+					<Helmet titleTemplate="%s :: Lightwell Survey Wines" />
+					<Grid.Container>
+						<Grid flex="0">
+							<Grid.Cell>
+								<Grid.Content>
+									<Header />
+								</Grid.Content>
+							</Grid.Cell>
+						</Grid>
+						<Grid>
+							<Grid.Cell>
+								<Routes>
+									<Route path="/" element={<IndexRoute />} />
+									<Route path="/shop" element={<ShopRoute />} />
+									<Route path="/about" element={<AboutRoute />} />
+									<Route path="/visit" element={<VisitRoute />} />
+									<Route path="/notes" element={<NotesRoute />} />
+									<Route path="/notes/:slug" element={<NotesSlugRoute />} />
+									<Route path="/contact" element={<ContactRoute />} />
+									<Route path="/legal" element={<LegalRoute />} />
+								</Routes>
+							</Grid.Cell>
+						</Grid>
+						<Grid flex="0">
+							<Grid.Cell>
+								<Grid.Content>
+									<Subscribe />
+								</Grid.Content>
+							</Grid.Cell>
+						</Grid>
+						<Grid flex="0">
+							<Grid.Cell>
+								<Grid.Content>
+									<Footer />
+								</Grid.Content>
+							</Grid.Cell>
+						</Grid>
+					</Grid.Container>
+				</Mq.Provider>
 			</BrowserRouter>
 		)
 	}
---
+++ src/components/Mq/index.js
@@ -0,0 +1,60 @@
+import { React } from '@/vendor'
+
+const Context = React.createContext()
+
+class Provider extends React.PureComponent {
+
+	state = {}
+
+	constructor (props) {
+		super(props)
+		for (const [screen, mq] of Object.entries(props.config)) {
+			const mql = window.matchMedia(mq)
+			this.state[screen] = mql.matches
+			mql.addListener((e) => {
+				this.setState({
+					[screen]: e.matches
+				})
+			})
+		}
+		this.setBodyClasses()
+	}
+
+	componentDidUpdate (props, state) {
+		this.setBodyClasses()
+	}
+
+	setBodyClasses () {
+		for(const [screen, enabled] of Object.entries(this.state)) {
+			const className = `screen-${screen}`
+			const method = (enabled) ? 'add' : 'remove'
+			document.body.classList[method](className)
+		}
+	}
+
+	render () {
+		return (
+			<Context.Provider value={this.state} children={this.props.children} />
+		)
+	}
+
+}
+
+class Mq extends React.PureComponent {
+
+	static Provider = Provider
+	static withMq = (Component) => (props) => (
+		<Mq>
+			{(mq) => (
+				<Component mq={mq} {...props} />
+			)}
+		</Mq>
+	)
+
+	render () {
+		return <Context.Consumer {...this.props} />
+	}
+
+}
+
+export default Mq
