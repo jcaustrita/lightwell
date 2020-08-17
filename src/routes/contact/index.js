e6978cd1d567927b5f55c9d386f35cde2804c0a2
Roi Kingon <rkingon@MacBook-Pro.local>
2020-08-17 07:59:59 -0700

    contact page content

--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -1,5 +1,5 @@
-import { React, PropTypes, Helmet } from '@/vendor'
-import { Grid, Masthead, Text } from '@/components'
+import { React, PropTypes, Helmet, Link } from '@/vendor'
+import { Mq, Grid, Masthead, Text } from '@/components'
 
 class ContactRoute extends React.PureComponent {
 
@@ -10,23 +10,62 @@ class ContactRoute extends React.PureComponent {
 				<Helmet>
 					<title>Contact</title>
 				</Helmet>
-				<Grid>
-					<Grid.Cell>
-						<Grid.Content>
-							<Masthead
-								background={require('./contact.png')}
-								foreground={<h2>Get In Touch</h2>}
-							/>
-						</Grid.Content>
-					</Grid.Cell>
-				</Grid>
-				<Grid>
-					<Grid.Cell>
-						<Grid.Content>
-							<Text>lorem ipsum...</Text>
-						</Grid.Content>
-					</Grid.Cell>
-				</Grid>
+				<Mq>
+					{(Mq) => (
+						<Grid vhFill={Mq.large}>
+							<Grid.Cell>
+								<Grid>
+									<Grid.Cell>
+										<Grid.Content>
+											<Masthead
+												background={require('./contact.png')}
+												foreground={<h2>Get In Touch</h2>}
+											/>
+										</Grid.Content>
+									</Grid.Cell>
+								</Grid>
+								<Grid stacked={!Mq.large}>
+									<Grid.Cell>
+										<Grid.Content>
+											<Text center={true}>
+												<h2>Where to Buy</h2>
+												<p>
+													Please contact:<br />
+													Williams Corner<br />
+													434.293.6082
+												</p>
+											</Text>
+										</Grid.Content>
+									</Grid.Cell>
+									<Grid.Cell>
+										<Grid.Content>
+											<Text center={true}>
+												<h2>Where to Buy</h2>
+												<p>
+													200 W. 12TH ST, UNIT# D53<br />
+													Waynesboro, VA. 22980<br />
+													<a href="mailto:wines@lightwellsurvey.com">wines@lightwellsurvey.com</a>
+												</p>
+											</Text>
+										</Grid.Content>
+									</Grid.Cell>
+									<Grid.Cell>
+										<Grid.Content>
+											<Text center={true}>
+												<h2>Support</h2>
+												<p>
+													Questions about your order?<br />
+													<a href="#">Shipping + Returns</a><br />
+													<Link to="/legal">Legal</Link>
+												</p>
+											</Text>
+										</Grid.Content>
+									</Grid.Cell>
+								</Grid>
+							</Grid.Cell>
+						</Grid>
+					)}
+				</Mq>
 			</>
 		)
 
