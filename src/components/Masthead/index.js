e71b2cabd467fce0033669de2908cd9955ee315e
Roi <rkingon@gmail.com>
2020-10-13 09:47:49 -0700

    Masthead Foreground Default

--- src/components/Masthead/index.js
+++ src/components/Masthead/index.js
@@ -1,7 +1,38 @@
 import { React, PropTypes } from '@/vendor'
 import styles from './styles.module.scss'
 
+class ForegroundDefault extends React.PureComponent {
+
+	static propTypes = {
+		headline: PropTypes.string,
+		callout: PropTypes.shape({
+			image: PropTypes.string,
+			text: PropTypes.string
+		})
+	}
+
+	render () {
+		return (
+			<>
+				<h2>{this.props.headline}</h2>
+				{this.props.callout && (
+					<div class={styles.callout}>
+						{this.props.callout.image && (
+							<img src={this.props.callout.image} />
+						)}
+						{this.props.callout.text}
+					</div>
+				)}
+			</>
+		)
+	}
+}
+
 class Masthead extends React.PureComponent {
+
+	static Foreground = {
+		Default: ForegroundDefault
+	}
 
 	static propTypes = {
 		background: PropTypes.string,
