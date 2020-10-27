7011a48dbdf6461e2e82b464757ece982d3f2e35
Roi <rkingon@gmail.com>
2020-10-27 09:17:48 -0700

    Homepage wireframe

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import { React, Helmet } from '@/vendor'
-import { Grid } from '@/components'
+import { Mq, Grid } from '@/components'
 
 class IndexRoute extends React.PureComponent {
 
@@ -10,13 +10,99 @@ class IndexRoute extends React.PureComponent {
 				<Helmet titleTemplate="">
 					<title>Lightwell Survey Wines :: Makin' Wine Straight Outta Virgina</title>
 				</Helmet>
-				<Grid>
-					<Grid.Cell>
-						<Grid.Content>
-							homepage...
-						</Grid.Content>
-					</Grid.Cell>
-				</Grid>
+				<Mq>
+					{(Mq) => (
+						<Grid stacked>
+							<Grid.Cell>
+								<Grid stacked={!Mq.large}>
+									<Grid.Cell>
+										<Grid stacked>
+											<Grid.Cell>
+												<Grid.Content>
+													[Shop rotator]
+												</Grid.Content>
+											</Grid.Cell>
+											<Grid.Cell flex={0}>
+												<Grid.Content>
+													[Free ground shipping]
+												</Grid.Content>
+											</Grid.Cell>
+										</Grid>
+									</Grid.Cell>
+									<Grid.Cell>
+										<Grid stacked>
+											<Grid.Cell flex={0}>
+												<Grid.Content pad={1}>
+													[Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.]
+												</Grid.Content>
+											</Grid.Cell>
+											<Grid.Cell>
+												<Grid stacked={Mq.small}>
+													<Grid.Cell>
+														<Grid stacked>
+															<Grid.Cell>
+																<Grid.Content pad={1}>
+																	[Notes Article]
+																</Grid.Content>
+															</Grid.Cell>
+															<Grid.Cell>
+																<Grid.Content pad={1}>
+																	[Our Story]
+																</Grid.Content>
+															</Grid.Cell>
+														</Grid>
+													</Grid.Cell>
+													<Grid.Cell>
+														<Grid.Content>
+															[Picture]
+														</Grid.Content>
+													</Grid.Cell>
+												</Grid>
+											</Grid.Cell>
+										</Grid>
+									</Grid.Cell>
+								</Grid>
+							</Grid.Cell>
+							<Grid.Cell>
+								<Grid stacked={!Mq.large}>
+									<Grid.Cell>
+										<Grid stacked>
+											<Grid.Cell>
+												<Grid.Content pad={1}>
+													[Tasting Room]
+												</Grid.Content>
+											</Grid.Cell>
+											<Grid.Cell>
+												<Grid.Content>
+													[Picture]
+												</Grid.Content>
+											</Grid.Cell>
+										</Grid>
+									</Grid.Cell>
+									<Grid.Cell>
+										<Grid.Content>
+											[Picture]
+										</Grid.Content>
+									</Grid.Cell>
+									<Grid.Cell>
+										<Grid stacked>
+											<Grid.Cell>
+												<Grid.Content pad={1}>
+													[Buy Wine]
+												</Grid.Content>
+											</Grid.Cell>
+											<Grid.Cell>
+												<Grid.Content>
+													[W]
+												</Grid.Content>
+											</Grid.Cell>
+										</Grid>
+									</Grid.Cell>
+								</Grid>
+							</Grid.Cell>
+						</Grid>
+					)}
+				</Mq>
 			</>
 		)
 
