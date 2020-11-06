2917ca3778864d87586622db04477a9b09f0bea8
Julian Caustrita <jcaustrita@gmail.com>
2020-11-05 19:17:35 -0800

    add 2018's to notes

---
+++ src/routes/notes/content/2018-los-idiots/index.js
@@ -0,0 +1,26 @@
+import { React } from '@/vendor'
+import { Button } from '@/components'
+
+export default {
+	title: '2018 Los Idiots',
+	date: '2020-11-11',
+	image: require('./2018-los-idiots@2x.jpg'),
+	body: (
+		<>
+			<p>Comin' soon...</p>
+			<p>
+				<strong>FRUIT SOURCES</strong><br />
+				N/A
+			</p>
+			<p>
+				<strong>WINEMAKING</strong><br />
+				N/A
+			</p>
+			<p>
+				<strong>CHEMISTRY</strong><br />
+				N/A
+			</p>
+		</>
+	),
+	cta: <Button.Link to="/shop">Shop Wines</Button.Link>
+}
---
+++ src/routes/notes/content/2018-the-weird-ones-are-wolves/index.js
@@ -0,0 +1,26 @@
+import { React } from '@/vendor'
+import { Button } from '@/components'
+
+export default {
+	title: '2018 The Weird Ones Are Wolves',
+	date: '2020-11-11',
+	image: require('./2018-the-weird-ones-are-wolves@2x.jpg'),
+	body: (
+		<>
+			<p>Comin' soon...</p>
+			<p>
+				<strong>FRUIT SOURCES</strong><br />
+				N/A
+			</p>
+			<p>
+				<strong>WINEMAKING</strong><br />
+				N/A
+			</p>
+			<p>
+				<strong>CHEMISTRY</strong><br />
+				N/A
+			</p>
+		</>
+	),
+	cta: <Button.Link to="/shop">Shop Wines</Button.Link>
+}
