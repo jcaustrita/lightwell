3a804dff5acadb5546d64d8b06612983a2255249
Julian <jcaustrita@gmail.com>
2021-02-06 09:21:03 -0800

    add tasting room link

--- src/routes/visit/index.js
+++ src/routes/visit/index.js
@@ -16,15 +16,14 @@ class VisitRoute extends React.PureComponent {
 							<Grid.Cell>
 								<Grid.Content pad={2}>
 									<Fs align="center" contained>
-										<Fs variant="h2" tag="h2">Schedule a Tasting</Fs>
-										<p>Join us in Waynesboro, VA (the Old Mill) for a look into our signature approach to both winemaking and frienship.</p>
-										<p>For all requests, please schedule your tasting by <a href="mailto:wines@lightwellsurvey.com">contacting us</a>. We are currently taking groups up to 6, this includes non-tasters.</p>
+										<Fs variant="h2" tag="h2">Visit Our Tasting Room</Fs>
+										<p>Join us in Waynesboro, VA (the Old Mill) for a look into our signature approach to both winemaking and frienship. We're currently doing wine tastings once a month with parties no larger than 6 people, this includes non-tasters. Dogs are welcome. If you have any questions, please reach out to us at <a href="mailto:wines@lightwellsurvey.com">wines@lightwellsurvey.com</a></p>
 										<p>
 											200 W. 12TH ST, UNIT# D53<br />
 											Waynesboro, VA. 22980
 										</p>
 										<p>
-											<a href="mailto:wines@lightwellsurvey.com">wines@lightwellsurvey.com</a>
+											<a href="https://square.site/book/LQFV0SYD1EBKS/lightwell-survey-wines-waynesboro-va">Book an Appointment</a>
 										</p>
 									</Fs>
 								</Grid.Content>
